f544906a06190d36643e69e7b18ae4443357db75
Felix Kirmse <23093465+FelixKirmse@users.noreply.github.com>
2012-09-19 16:05:45 +0200

    Forgot to close a comment, whoopsie!

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -2,6 +2,9 @@ var searchData=
 [
   ['closeapp',['CloseApp',['../structMansionEscape_1_1GameOptions.html#a5999d45c099cebff88458d4e9ba86030',1,'MansionEscape::GameOptions']]],
   ['commentonitem',['CommentOnItem',['../classMansionEscape_1_1Controller.html#a78a207c6175f140266c3941c6539b6bb',1,'MansionEscape::Controller::CommentOnItem()'],['../classMansionEscape_1_1IController.html#a3c9e2216e8de76641068b74ea778af52',1,'MansionEscape::IController::CommentOnItem()']]],
+  ['contextaction',['ContextAction',['../structMansionEscape_1_1ContextAction.html',1,'MansionEscape']]],
+  ['contextitem',['ContextItem',['../structMansionEscape_1_1ContextAction.html#a186eef2eeee01babc2aa97a00f5425db',1,'MansionEscape::ContextAction']]],
+  ['contexttype',['ContextType',['../structMansionEscape_1_1ContextAction.html#a5d40c08399830a05fe973a9d3e40f4d8',1,'MansionEscape::ContextAction']]],
   ['controller',['Controller',['../classMansionEscape_1_1Controller.html',1,'MansionEscape']]],
   ['currentroom',['CurrentRoom',['../structMansionEscape_1_1UIPackage.html#a756dca653306bdd163f81464318c80b8',1,'MansionEscape::UIPackage']]]
 ];
