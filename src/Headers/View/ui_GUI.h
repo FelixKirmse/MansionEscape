f2f53abfceb211eab70cdb5c67bc92b3d493fc61
Tobias Wegner <seth.god@gmx.net>
2012-11-05 09:48:03 +0100

    GUI/TUI Update

--- src/Headers/View/ui_GUI.h
+++ src/Headers/View/ui_GUI.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'GUI.ui'
 **
-** Created: Mon Nov 5 09:04:06 2012
+** Created: Mon Nov 5 09:47:28 2012
 **      by: Qt User Interface Compiler version 4.8.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -16,8 +16,13 @@
 #include <QtGui/QButtonGroup>
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
+#include <QtGui/QListView>
 #include <QtGui/QMainWindow>
+#include <QtGui/QMenuBar>
 #include <QtGui/QPushButton>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTextBrowser>
+#include <QtGui/QToolBar>
 #include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,24 +30,77 @@ QT_BEGIN_NAMESPACE
 class Ui_GUI
 {
 public:
-    QWidget *dockWidgetContents;
-    QPushButton *TestButton;
-    QLabel *label;
+    QWidget *centralWidget;
+    QPushButton *_forward;
+    QPushButton *_turnLeft;
+    QPushButton *_turnRight;
+    QListView *_inventarBar;
+    QTextBrowser *_textOutput;
+    QPushButton *_action2;
+    QPushButton *_action1;
+    QPushButton *_action3;
+    QPushButton *_action4;
+    QPushButton *_action3_2;
+    QPushButton *_action3_4;
+    QLabel *_imageFrame;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
 
     void setupUi(QMainWindow *GUI)
     {
         if (GUI->objectName().isEmpty())
             GUI->setObjectName(QString::fromUtf8("GUI"));
-        GUI->resize(943, 567);
-        dockWidgetContents = new QWidget(GUI);
-        dockWidgetContents->setObjectName(QString::fromUtf8("dockWidgetContents"));
-        TestButton = new QPushButton(dockWidgetContents);
-        TestButton->setObjectName(QString::fromUtf8("TestButton"));
-        TestButton->setGeometry(QRect(320, 290, 171, 27));
-        label = new QLabel(dockWidgetContents);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(250, 260, 341, 17));
-        GUI->setCentralWidget(dockWidgetContents);
+        GUI->resize(640, 480);
+        centralWidget = new QWidget(GUI);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        _forward = new QPushButton(centralWidget);
+        _forward->setObjectName(QString::fromUtf8("_forward"));
+        _forward->setGeometry(QRect(130, 100, 51, 51));
+        _turnLeft = new QPushButton(centralWidget);
+        _turnLeft->setObjectName(QString::fromUtf8("_turnLeft"));
+        _turnLeft->setGeometry(QRect(70, 150, 51, 51));
+        _turnRight = new QPushButton(centralWidget);
+        _turnRight->setObjectName(QString::fromUtf8("_turnRight"));
+        _turnRight->setGeometry(QRect(190, 150, 51, 51));
+        _inventarBar = new QListView(centralWidget);
+        _inventarBar->setObjectName(QString::fromUtf8("_inventarBar"));
+        _inventarBar->setGeometry(QRect(10, 330, 621, 81));
+        _textOutput = new QTextBrowser(centralWidget);
+        _textOutput->setObjectName(QString::fromUtf8("_textOutput"));
+        _textOutput->setGeometry(QRect(10, 250, 621, 71));
+        _action2 = new QPushButton(centralWidget);
+        _action2->setObjectName(QString::fromUtf8("_action2"));
+        _action2->setGeometry(QRect(20, 110, 98, 27));
+        _action1 = new QPushButton(centralWidget);
+        _action1->setObjectName(QString::fromUtf8("_action1"));
+        _action1->setGeometry(QRect(110, 210, 98, 27));
+        _action3 = new QPushButton(centralWidget);
+        _action3->setObjectName(QString::fromUtf8("_action3"));
+        _action3->setGeometry(QRect(20, 60, 271, 27));
+        _action4 = new QPushButton(centralWidget);
+        _action4->setObjectName(QString::fromUtf8("_action4"));
+        _action4->setGeometry(QRect(190, 110, 98, 27));
+        _action3_2 = new QPushButton(centralWidget);
+        _action3_2->setObjectName(QString::fromUtf8("_action3_2"));
+        _action3_2->setGeometry(QRect(20, 30, 271, 27));
+        _action3_4 = new QPushButton(centralWidget);
+        _action3_4->setObjectName(QString::fromUtf8("_action3_4"));
+        _action3_4->setGeometry(QRect(20, 0, 271, 27));
+        _imageFrame = new QLabel(centralWidget);
+        _imageFrame->setObjectName(QString::fromUtf8("_imageFrame"));
+        _imageFrame->setGeometry(QRect(310, 0, 320, 240));
+        GUI->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(GUI);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 640, 25));
+        GUI->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(GUI);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        GUI->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(GUI);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        GUI->setStatusBar(statusBar);
 
         retranslateUi(GUI);
 
@@ -51,9 +109,17 @@ public:
 
     void retranslateUi(QMainWindow *GUI)
     {
-        GUI->setWindowTitle(QApplication::translate("GUI", "DockWidget", 0, QApplication::UnicodeUTF8));
-        TestButton->setText(QApplication::translate("GUI", "Push me to test slots", 0, QApplication::UnicodeUTF8));
-        label->setText(QApplication::translate("GUI", "This label should change once you push the button.", 0, QApplication::UnicodeUTF8));
+        GUI->setWindowTitle(QApplication::translate("GUI", "Mansion Escape", 0, QApplication::UnicodeUTF8));
+        _forward->setText(QApplication::translate("GUI", "\303\216", 0, QApplication::UnicodeUTF8));
+        _turnLeft->setText(QApplication::translate("GUI", "<-", 0, QApplication::UnicodeUTF8));
+        _turnRight->setText(QApplication::translate("GUI", "->", 0, QApplication::UnicodeUTF8));
+        _action2->setText(QApplication::translate("GUI", "Untersuche", 0, QApplication::UnicodeUTF8));
+        _action1->setText(QApplication::translate("GUI", "Benutze", 0, QApplication::UnicodeUTF8));
+        _action3->setText(QApplication::translate("GUI", "Nehme Kondom", 0, QApplication::UnicodeUTF8));
+        _action4->setText(QApplication::translate("GUI", "Kommentiere", 0, QApplication::UnicodeUTF8));
+        _action3_2->setText(QApplication::translate("GUI", "Nehme Staubsaugerschl\303\274ssel", 0, QApplication::UnicodeUTF8));
+        _action3_4->setText(QApplication::translate("GUI", "Nehme Vase", 0, QApplication::UnicodeUTF8));
+        _imageFrame->setText(QString());
     } // retranslateUi
 
 };
--- src/MansionEscape/View/moc_GUI.cpp
+++ src/MansionEscape/View/moc_GUI.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'GUI.h'
 **
-** Created: Mon Nov 5 09:04:06 2012
+** Created: Mon Nov 5 09:47:28 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,33 +23,25 @@ static const uint qt_meta_data_MansionEscape__GUI[] = {
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
- // slots: signature, parameters, type, tag, flags
-      20,   19,   19,   19, 0x08,
-
        0        // eod
 };
 
 static const char qt_meta_stringdata_MansionEscape__GUI[] = {
-    "MansionEscape::GUI\0\0on_TestButton_pressed()\0"
+    "MansionEscape::GUI\0"
 };
 
 void MansionEscape::GUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        Q_ASSERT(staticMetaObject.cast(_o));
-        GUI *_t = static_cast<GUI *>(_o);
-        switch (_id) {
-        case 0: _t->on_TestButton_pressed(); break;
-        default: ;
-        }
-    }
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
     Q_UNUSED(_a);
 }
 
@@ -84,11 +76,6 @@ int MansionEscape::GUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QMainWindow::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
-    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
